3b0266ac476a86c180b52b3acab31b577b40d680
Dmitry <1Google2@mail.ru>
2019-10-23 15:26:55 +1000

    All works, need second stepper and ratio 1:24

--- Driver/Driver.ino
+++ Driver/Driver.ino
@@ -9,12 +9,10 @@
 #include "USART.h"
 #include "Timer0.h"
 #include "Timer1.h"
-unsigned char data[10]={0};
-
 
 
 Time timer1=0;
-int main(){
+int main(void){
 
   DDRC&=~((1<<FORWARD)|(1<<BACKWARD)|(1<<CW)|(1<<CCW));
   PORTC|=(1<<FORWARD)|(1<<BACKWARD)|(1<<CW)|(1<<CCW);
@@ -31,9 +29,9 @@ int main(){
   sei();
   for(int i=0;;){
     if(USART_Available()){
+      static unsigned char data[10]={0};
       char temp=USART_Receive();
       if(temp=='\n' || temp=='\r'){
-        USART_Transmit(counter+'0');
         i=0;
         Timer1_RotateSteps(0);
         Timer1_Disable();
@@ -65,15 +63,22 @@ int main(){
         }else if (curr_flags[1]==1 && prev_flags[1]==0){
           long rotation_step=0;
           if((counter-1)%7==0){
-            rotation_step=58;
+            rotation_step=-58;
             }else{
-              rotation_step=57;
+              rotation_step=-57;
             }
           Timer1_Disable();
-          Timer1_RotateSteps(-57);
+          Timer1_RotateSteps(rotation_step);
           Timer1_Enable();
           counter--;
         }
+        char buf[10]={0};
+        sprintf(buf,"%d",counter);
+        for(int l=0;l<10;USART_Transmit(buf[l++]));
+        USART_Transmit(' ');
+        sprintf(buf,"%d",counter-1);
+        for(int l=0;l<10;USART_Transmit(buf[l++]));
+        USART_Transmit('\n');
         if(curr_flags[2]==1){ //Linear Buttons
           Timer1_LinearMove(1);
         }else if(curr_flags[3]==1){
